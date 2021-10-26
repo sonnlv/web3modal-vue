1779d9e530751c9d28e3f3563870e782d88c827e
Jae <son92wru@gmail.com>
2021-10-26 11:43:43 +0700

    init

--- src/providers/connectors/index.js
+++ src/providers/connectors/index.js
@@ -1,8 +1,25 @@
 import injected from "./injected";
 import walletconnect from "./walletconnect";
-
+import portis from "./portis";
+import fortmatic from "./fortmatic";
+import arkane from "./arkane";
+import authereum from "./authereum";
+import burnerconnect from "./burnerconnect";
+import mewconnect from "./mewconnect";
+import dcentwallet from "./dcentwallet";
+import bitski from "./bitski";
+import frame from "./frame";
 
 export {
     injected,
     walletconnect,
+    portis,
+    fortmatic,
+    arkane,
+    authereum,
+    burnerconnect,
+    mewconnect,
+    dcentwallet,
+    bitski,
+    frame
 };
